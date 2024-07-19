8cfa5fe74782ec0407bcf216af72dbc6042378eb
David Altamirano <jdaltamiranodev@gmail.com>
2024-07-19 12:52:28 +0200

    Se añade el proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/components/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroModule } from './heroes/hero.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CharacterComponent } from './components/character/character.component';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+    ListComponent,
+    CharacterComponent
+  ],
+  exports: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+
+export class DbzModule { }
